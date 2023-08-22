ec47fd554ecaf3f5a36accbdf1c30c31ebcfad8b
Jayesh <jaykumar25012001@gmail.com>
2023-08-22 19:05:47 +0530

    #18

--- 01.Langchain_Intro/04.huggingface_test.py
+++ 01.Langchain_Intro/04.huggingface_test.py
@@ -6,13 +6,14 @@ HUGGINGFACEHUB_API_KEY = os.getenv("HUGGINGFACEHUB_API_KEY")
 
 from langchain.llms import HuggingFaceHub
 
-repo_id = "google/flan-t5-xxl"
+# repo_id = "google/flan-t5-xxl"
+repo_id = "google/flan-t5-xl"
 # repo_id = "databricks/dolly-v2-3b"
 # repo_id = "databricks/dolly-v2-12b"
 
 # Initialize Hugging Face LLM without the api_key parameter
-llm_huggingface = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.9, "max_length": 64})
+llm_huggingface = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0, "max_length": 64})
 
-our_query = "Who won ipl 2023 ?"
+our_query = "What is area 51 ?"
 completion_huggingface = llm_huggingface(our_query)
 print(completion_huggingface)
